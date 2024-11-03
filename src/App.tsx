8bebdff4844281669584a4e20facccd09903c64f
Toaster <prokhorov.rostislav@gmail.com>
2024-11-03 14:14:02 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react"
+
+const App = () => {
+  return (
+    <div>
+      <Button color="primary">213</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/app/services/api.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query"
+import { BASE_URL } from "../../constants"
+import { RootState } from "../store"
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${BASE_URL}/api`,
+  prepareHeaders: (Headers, { getState }) => {
+    const token =
+      (getState() as RootState).auth.token || localStorage.getItem("token")
+
+    if (token) {
+      Headers.set("authorisation", `Bearer ${token}`)
+    }
+    return Headers
+  },
+})
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 })
+export const api = createApi({
+  reducerPath: "splitApi",
+  baseQuery: baseQueryWithRetry,
+  refetchOnMountOrArgChange: true,
+  endpoints: () => ({}),
+})
